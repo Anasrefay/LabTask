17b79a552fd991095223c213b26d773d446d55ad
Anas_Refaei <anasrefay0177@gmail.com>
2025-05-07 19:27:47 +0300

    20230072

--- Task.java
+++ Task.java
@@ -1,8 +1,8 @@
 public class Task {
     public static void main(String[] args) {
-        System.out.println("Name: Anas Refaei ID:20230072");
-        System.out.println("Name: Omar Awad ID:20230260");
-        System.out.println("Name: Amr Khaled ID:20230273");
+        System.out.println("Name: Anas Refaei and ID:20230072");
+        System.out.println("Name: Omar Awad and ID:20230260");
+        System.out.println("Name: Amr Khaled and ID:20230273");
     }
 }
 
